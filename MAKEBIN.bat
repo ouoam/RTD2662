d2c0d631fd773250e6ab376ec05ab04f8bf91dad
Greg Willard (r3pwn) <wickett06@gmail.com>
2018-08-31 09:32:59 -0400

    Move build artifacts to Bin

--- MAKEBIN.bat
+++ MAKEBIN.bat
@@ -1,1 +1,6 @@
-CreateBin.exe Firmware.bin Output\RTD2662.H00 Output\RTD2662.H01
+@echo off
+mkdir Bin > nul
+del Bin\*
+copy Output\RTD2662.H00 Bin\RTD2662.H00
+copy Output\RTD2662.H00 Bin\RTD2662.H01
+CreateBin.exe Bin\Firmware.bin Output\RTD2662.H00 Output\RTD2662.H01
