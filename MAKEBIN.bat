b6da5094e256bd27bb50a701362fc27b48d721d1
Greg Willard (r3pwn) <wickett06@gmail.com>
2018-08-31 09:40:55 -0400

    make batch scripts cleanup out dir every build, as well as print resulting file location

--- MAKEBIN.bat
+++ MAKEBIN.bat
@@ -1,6 +1,8 @@
 @echo off
 mkdir Bin > nul
-del Bin\*
+del Bin\* /F /Q
 copy Output\RTD2662.H00 Bin\RTD2662.H00
 copy Output\RTD2662.H00 Bin\RTD2662.H01
 CreateBin.exe Bin\Firmware.bin Output\RTD2662.H00 Output\RTD2662.H01
+echo.
+echo --- Resulting file is Bin\Firmware.bin. ---
--- Merge128K.bat
+++ Merge128K.bat
@@ -1,6 +1,9 @@
 @echo off
 mkdir Bin > nul
+del Bin\* /F /Q
 copy Output\RTD2662.H00 Bin\RTD2662.H00
 copy Output\RTD2662.H01 Bin\RTD2662.H01
 HEX2BIN /Q /L131072   Bin\RTD2662.H00 Bin\RTD2662.BIN
 HEX2BIN /Q /O65536 /M Bin\RTD2662.H01 Bin\RTD2662.BIN
+echo.
+echo --- Resulting file is Bin\Firmware.bin. ---
