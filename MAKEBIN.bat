0959ad61c10fbcb6bcc3fbb3772cb5d5a0449261
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-06-01 18:51:03 +0700

    Fix copy file

--- MAKEBIN.bat
+++ MAKEBIN.bat
@@ -2,7 +2,7 @@
 mkdir Bin > nul
 del Bin\* /F /Q
 copy Output\RTD2662.H00 Bin\RTD2662.H00
-copy Output\RTD2662.H00 Bin\RTD2662.H01
+copy Output\RTD2662.H01 Bin\RTD2662.H01
 CreateBin.exe Bin\Firmware.bin Output\RTD2662.H00 Output\RTD2662.H01
 echo.
 echo --- Resulting file is Bin\Firmware.bin. ---
